c8f8de7e170a3daffbc541b0db51fd9902c7aeb2
sextynine <mohammad87115@gmail.com>
2024-12-26 16:36:55 +0330

    Implement grow command and update Dick schema; enhance error handling and response messages

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -14,10 +14,13 @@ module.exports = {
             try {
                 await command.execute(interaction);
             } catch (error) {
+                let msg = 'Error while executing command'
+
+                if (error.rawError?.message === 'Missing Permissions' || error.rawError?.message === 'Message was blocked by AutoMod') msg += '\n\nNote that this error can be caused by the automoderation system filtering bots messages (you can use the ephemeral mode if thats the case)'
                 if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({ content: 'Error while executing command', ephemeral: true });
+                    await interaction.followUp({ content: msg, ephemeral: true });
                 } else {
-                    await interaction.reply({ content: 'Error while executing command', ephemeral: true });
+                    await interaction.reply({ content: msg, ephemeral: true });
                 }
                 logger.error(error)
                 console.error(error)
