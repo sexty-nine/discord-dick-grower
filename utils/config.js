6e5b63dde01f204a63a1161fc6266bcd81a710ac
sextynine <mohammad87115@gmail.com>
2024-12-27 15:21:13 +0330

    Add ownerId to configuration: include OWNER_ID from environment variables

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 module.exports = {
     token: process.env.TOKEN,
     databaseUri: process.env.DATABASE_URI,
+    ownerId: process.env.OWNER_ID
 }
 
 // just for not using process.env thing
