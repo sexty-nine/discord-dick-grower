9c229fa8646c0e752fe5def14b122be55d766a68
Neoriat <Neoriat@duck.com>
2024-12-28 18:09:34 +0330

    Removed the unnecessary option(command) Implemented the set-size command

--- commands/slash/Admin/admin.js
+++ commands/slash/Admin/admin.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder, ApplicationIntegrationType, InteractionContextType } = require('discord.js');
 const slashCommandLoader = require('../../../handlers/client/slashCommands.js')
+const Dick = require('../../../database/schemas/Dick.js')
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('admin')
@@ -9,40 +11,75 @@ module.exports = {
             .addSubcommand((sub) => sub
                 .setName('reload')
                 .setDescription('Reloads a command!')
-
-                .addStringOption(option => 
-                    option
-                        .setName('command')    
-                        .setDescription('The command to be restarted')
-                        .setRequired(true),
-        
-                    )
                 .addBooleanOption(option => option
                     .setName('ephemeral')
                     .setDescription('Only you can see the response')
                 )
-            ),
+            ).addSubcommand(sub => sub
+                .setName('set-size')
+                .setDescription('Set the size of the dick of a user!')
+                .addUserOption(option => option
+                    .setName('user')
+                    .setDescription('The user you want to set the size for')
+                    .setRequired(true)
+                )
+                .addIntegerOption(option => option
+                    .setName('size')
+                    .setDescription('The size you want to set the dick to')
+                    .setRequired(true)
+                )
+            )
+            ,
+
 
         async execute(interaction) {
 
             const subCommand = interaction.options.getSubcommand();
 
+            // Checks if the user is the owner or not
+            if (interaction.user.id !== interaction.client.config.ownerId) return await interaction.editReply("No")
+
+            const ephemeral = interaction.options.getBoolean('ephemeral');
+            await interaction.deferReply({ ephemeral });
+
             switch (subCommand){
                 case 'reload':
-                    const ephemeral = interaction.options.getBoolean('ephemeral');
-                    await interaction.deferReply({ ephemeral });
-            
-                    // Checks if the user is the owner or not
-                    if (interaction.user.id !== interaction.client.config.ownerId) return await interaction.editReply("No")
-            
+
                     await slashCommandLoader(interaction.client)
                     
-                    const embed = new EmbedBuilder()
+                    const reloadEmbed = new EmbedBuilder()
                         .setTitle('Reloaded')
                         .setFooter({ text: `Requested by ${interaction.member?.nickname || interaction.user.displayName}`, iconURL: interaction.client.user.displayAvatarURL() })
                         .setTimestamp()
             
-                    await interaction.editReply({ embeds: [embed] })
+                    await interaction.editReply({ embeds: [reloadEmbed] })
+
+                case 'set-size':
+
+                    const chatId = interaction.guildId ?? interaction.channelId;
+                    const userId = interaction.user.id;
+                    const size = interaction.options.getInteger('size');
+
+                    const dick = await Dick.findOne({
+                        chatId:chatId,
+                        userId: userId,
+                    }).exec()
+
+                    await dick.updateOne({
+                        size:size
+                    })
+
+                    const sizeSetEmbed = new EmbedBuilder()
+                        .setTitle('Success')
+                        .setDescription(`You successfully set the dick size of <@${interaction.user.id}> to **${size}**`)
+                        .setFooter({ text: `Requested by ${interaction.member?.nickname || interaction.user.displayName}`, iconURL: interaction.client.user.displayAvatarURL() })
+                        .setTimestamp()
+
+                    await interaction.editReply({ embeds:[sizeSetEmbed] })
+
+                    
+                    
+
             }
         }
             
