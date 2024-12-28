250bd1c02a9e89bd8d14873646c18ea05a042433
Neoriat <Neoriat@duck.com>
2024-12-28 13:25:40 +0330

    Increased randomness in the battle algorithm

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,21 @@
 module.exports = {
     calculateBattleResult: (user, opponent, userDick, opponentDick) => {
+        // Win rate is based on the size of the dicks
         const userWinRate = userDick.size / (userDick.size + opponentDick.size) * 100;
         const opponentWinRate = opponentDick.size / (userDick.size + opponentDick.size) * 100;
-        const randomFactor = Math.round(Math.random() * 2) - 1;
+        // Generates a number between 0 and 9 | This will add some randomness to the algorithm
+        const userRandomFactor = Math.floor(Math.random() * 10);
+        const opponentRandomFactor = Math.floor(Math.random() * 10);
+        // Will decrease the total win rate by 5 if the player has more than 5 streak 
         const userStreakAdjustment = Math.min(userDick.loseStreak, 5) - Math.min(userDick.winStreak, 5);
         const opponentStreakAdjustment = Math.min(opponentDick.loseStreak, 5) - Math.min(opponentDick.winStreak, 5);
-        const adjustedUserWinRate = userWinRate + randomFactor + userStreakAdjustment;
-        const adjustedOpponentWinRate = opponentWinRate - randomFactor + opponentStreakAdjustment;
+        // Determines the win rate based on the the three inputs above 
+        // These are the values that the users see as win rates
+        const adjustedUserWinRate = userWinRate + userStreakAdjustment + userRandomFactor;
+        const adjustedOpponentWinRate = opponentWinRate + opponentStreakAdjustment + opponentRandomFactor;
     
-        const randomChance = Math.random() * 100;
-        const winner = randomChance < adjustedUserWinRate ? user : opponent;
+        // Whoever has the highest win rate wins
+        const winner = adjustedOpponentWinRate < adjustedUserWinRate ? user : opponent;
     
         return {
             user: user.username,
